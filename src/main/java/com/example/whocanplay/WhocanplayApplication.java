d33f8a479377a1167fe64d123b1a50119370868a
Caroline-Oliver <me@carolineoliver.net>
2023-10-13 14:31:51 -0500

    initial commit

---
+++ src/main/java/com/example/whocanplay/WhocanplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.whocanplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhocanplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhocanplayApplication.class, args);
+	}
+
+}
