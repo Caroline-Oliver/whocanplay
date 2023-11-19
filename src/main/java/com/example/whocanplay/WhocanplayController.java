5e2e3f0dcf60a882de90a4387fc117d9d7cd682e
Christian Walker <christiandwalker2021@gmail.com>
2023-11-19 16:17:40 -0600

    Added filter route

--- src/main/java/com/example/whocanplay/WhocanplayController.java
+++ src/main/java/com/example/whocanplay/WhocanplayController.java
@@ -37,6 +37,8 @@ public class WhocanplayController {
     private final String SISTERLOCATIONURL = "https://cdn.cloudflare.steamstatic.com/steam/apps/506610/header.jpg?t=1579635985";
     private final String FALLGUYSURL = "https://upload.wikimedia.org/wikipedia/en/d/d4/Fall_Guys_Post_F2P_keyart.png";
 
+    //Upon assignment, we need to make a request and get all of our possible parameters. This way we can just reference them
+
     @Autowired
     public WhocanplayController(LRUCache lruCache){
        this.setLruCache(lruCache);
@@ -95,6 +97,8 @@ public class WhocanplayController {
 
     //TODO: This will make the request to the SQL Server
     public List<Map<String,String>> makeSearchRequest(Map<String,String> args){
+        //NOTE: Basically all of this logic will eventually be replaced by the SQL
+
         List<Map<String,String>> games = new ArrayList<>();
         for(int i = 0; i < 4;i++){
             games.addAll(List.of(
@@ -102,11 +106,11 @@ public class WhocanplayController {
                             "Version 12","4 hardware CPU Threads Intel Core i5 750 or higher", """
                             Explore large, destructible environments where no two games are ever the same. Team up with friends by sprinting, climbing and smashing your way to earn your Victory Royale, whether you choose to build up in Fortnite Battle Royale or go no-builds in Fortnite Zero Build.
                             Discover even more ways to play across thousands of creator-made game genres: adventure, roleplay, survival and more. Or, band together with up to three friends to fend off hordes of monsters in Save the World.
-                            ""","80%", FORTNITEURL).getGameData(),
+                            ""","80", FORTNITEURL).getGameData(),
 
                     new Game("Minecraft","Geforce infinity",
                             "version 12","Intel core processor 502940","Minecraft is a game made up of blocks, creatures, and community. Blocks can be used to reshape the world or build fantastical creations. Creatures can be battled or befriended, depending on your playstyle. Experience epic adventures solo or with friends, there’s no wrong way to play.\n" +
-                            "Unless you’re digging straight down.","70%", MINECRAFTURL).getGameData(),
+                            "Unless you’re digging straight down.","70", MINECRAFTURL).getGameData(),
 
                     new Game("Fall Guys","Xbox 360 or sum shit","Version 12",
                             "Intel Core process 45", """
@@ -114,20 +118,42 @@ public class WhocanplayController {
                             Create your own Course: Fall Guys Creative is a level editor that allows you to create fiendish custom Rounds and share them with the wider community.
                             Competitive & Cooperative: Tumble between competitive free-for-alls and cooperative challenges—or take on the Blunderdome with up to 3 friends!
                             Play with Friends: Fall Guys supports cross-play, cross-platform parties and cross-progression via your Epic Games Account.
-                            Ever-Evolving Content: Play stays fresh with new collabs and game updates which bring new Costumes, Obstacles, and ways to play.""","80%", FALLGUYSURL).getGameData(),
+                            Ever-Evolving Content: Play stays fresh with new collabs and game updates which bring new Costumes, Obstacles, and ways to play.""","80", FALLGUYSURL).getGameData(),
 
                     new Game("Five Night's at Freddy's: Sister Location","GeForce GTX 6GB","Version 12",
-                            "A random processor","Welcome to Circus Baby's Pizza World, where family fun " +
+                            "AMD Athlon","Welcome to Circus Baby's Pizza World, where family fun " +
                             "and interactivity go beyond anything you've seen at those *other* pizza places! Now hiring: " +
                             "Late night technician. Must enjoy cramped spaces and be comfortable around active machinery. " +
-                            "Not responsible for death or dismemberment.","100%", SISTERLOCATIONURL).getGameData()
+                            "Not responsible for death or dismemberment.","100", SISTERLOCATIONURL).getGameData()
             ));
         }
-
+        Game roblox = new Game("Roblox","GeForce GTX 6GB","Version 12",
+                "AMD Athlon","Roblox is an online game platform and game creation system developed " +
+                "by Roblox Corporation that allows users to program games and play games created by other users.","20",
+                "https://images.rbxcdn.com/c83761712c58384892d63501cad3a1ee");
+        games.add(roblox.getGameData());
 
         //PLAN: Add to check if we actually have arguments. If not, or they are blank, we return everything
+
         return (null == args) ? games : games.stream()
                 .filter(obj-> obj.get("name").equalsIgnoreCase(args.get("name")))
                 .collect(Collectors.toList());
+    }
+
+    //FUNC: All that this route does is make a request to the SQL database to get a collection of all of the possible processors and graphics cards
+    @GetMapping("/filters")
+    public Map<String,List<String>> Filters(){
+
+        //TODO: We need an ordered list so that we can properly parse this stuff so that we can guarantee that a
+
+        //NOTE: These will of course be later replaced by a SQL query
+        List<String> graphicsList = List.of("GeForce GTX 6GB","Geforce infinity","Xbox 360 or sum shit","GeForce GTX 6GB");
+        List<String> processorList = List.of("Intel Core i5","Intel core processor 502940","Intel Core process 45","AMD Athlon");
+
+        return Map.of(
+            "processors",processorList,
+                "graphics",graphicsList
+        );
+
     }
 }
